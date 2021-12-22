79ee4687ce2edc81c2e68efffdb07dd2d610bd72
Juan Jose Mayorga <devjuanjose@gmail.com>
2021-12-22 15:52:45 -0600

    ref added to text input on toolbar component

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+interface ToolbarProps {
+  idFocused: boolean;
+  onChangeFocus?: (value: boolean) => void;
+  onSubmit?: () => void;
+  onPressCamera?: () => void;
+  onPressLocation?: () => void;
+}
+
+interface ToolbarButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+interface IToolbarState {
+  text: string;
+}
+
+const initialState = {
+  text: '',
+}
+
+const ToolbarButton = ({title, onPress}: ToolbarButtonProps) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.button}>{title}</Text>
+  </TouchableOpacity>
+);
+
+export const Toolbar = ({
+  idFocused,
+  onChangeFocus = () => { /* This is an intentional empty function */ },
+  onSubmit = () => { /* This is an intentional empty function */ },
+  onPressCamera = () => { /* This is an intentional empty function */ },
+  onPressLocation = () => { /* This is an intentional empty function */ },
+}: ToolbarProps) => {
+
+  const [{ text }, setState] = useState<IToolbarState>(initialState)
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    console.log('Component mounted')
+  }, [])
+
+  const handleChangeText = (text: string) => {
+    setState({ text })
+  }
+
+  const handleSubmitEditing = () => {
+    if (!text) return;
+
+    onSubmit();
+    setState(initialState);
+  }
+
+  const handleFocus = () => {
+    onChangeFocus(true);
+  }
+
+  const handleBlur = () => {
+    onChangeFocus(false);
+  }
+
+  // const setInputRef = (ref: any) => {
+
+  // }
+
+  return (
+    <View style={styles.toolbar}>
+      <ToolbarButton title={'C'} onPress={onPressCamera} />
+      <ToolbarButton title={'L'} onPress={onPressLocation} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid='transparent'
+          placeholder='Type something'
+          blurOnSubmit={false}
+          value={text}
+          onChangeText={handleChangeText}
+          onSubmitEditing={handleSubmitEditing}
+          ref={inputRef}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  toolbar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    paddingLeft: 16,
+    backgroundColor: 'white',
+  },
+  button: {
+    top: -2,
+    marginRight: 12,
+    fontSize: 20,
+    color: 'grey',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.04)',
+    borderRadius: 16,
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    backgroundColor: 'rgba(0,0,0,0.02)',
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+  },
+  // ...
+});
